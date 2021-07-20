e6356191acaad2104e613ed6e96e72ea14544fd4
Ryan Puhalovich <reanpuhalovich@gmail.com>
2021-07-20 19:28:23 +1000

    Fixed wrong path in setup.bat.

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -3,5 +3,5 @@ cd /D "%~dp0"
 call git clone --recursive https://github.com/steinbergmedia/vst3projectgenerator.git ..\extern\vst3projectgenerator
 call cmake -S ../extern/vst3projectgenerator -B ../extern/vst3projectgenerator/build -G"Visual Studio 16 2019"
 call cmake --build ../extern/vst3projectgenerator/build/
-..\extern\vst3projectgenerator\build\Debug\VST3_Project_Generator.exe
+..\extern\vst3projectgenerator\build\Debug\VST3_Project_Generator\VST3_Project_Generator.exe
 cd ..
